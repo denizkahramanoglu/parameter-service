bedcb0b81cb120b62b1d17a97b21586de58ad0c6
deniz <denizkahramanoglu@gmail.com>
2026-07-17 09:57:13 +0300

    Initial commit

---
+++ src/main/java/com/example/parameter_service/ParameterServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.parameter_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParameterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParameterServiceApplication.class, args);
+	}
+
+}
