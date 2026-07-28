a04045ada78598873825e9a39670a0aeb7c833cf
deniz <denizkahramanoglu@gmail.com>
2026-07-28 14:01:37 +0300

    coverage hesaplama

---
+++ src/test/java/com/example/parameter_service/ParameterServiceApplicationTest.java
@@ -0,0 +1,28 @@
+package com.example.parameter_service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.springframework.boot.SpringApplication;
+
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.times;
+
+class ParameterServiceApplicationTest {
+
+    @Test
+    @DisplayName("Uygulama main metodu çağrıldığında SpringApplication.run tetiklenmeli")
+    void main_shouldCallSpringApplicationRun() {
+        try (MockedStatic<SpringApplication> mockedSpringApplication = mockStatic(SpringApplication.class)) {
+
+            String[] args = new String[]{};
+
+            ParameterServiceApplication.main(args);
+
+            mockedSpringApplication.verify(
+                    () -> SpringApplication.run(ParameterServiceApplication.class, args),
+                    times(1)
+            );
+        }
+    }
+}
